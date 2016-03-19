cda3e22ec138234038efdc5e67f5ff66766049b2
Andrew Haine <andrewhaine0@gmail.com>
2016-03-19 21:16:07 +0000

    Finished javascript to get and append message
    Main javascript complete, used jQuery to append the message to the feed,
    completed sass/css animations for the resize on scroll

--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@ window.onload = function () {
     var mediaExpand = document.getElementById("media-expand-arrow");
     var mediaBar = document.getElementById("media-bar");
     var mediaCross = document.getElementById("media-bar-cross");
+    var messageFeed = document.getElementById("message-feed");
     
     console.log(mediaExpand + mediaBar);
     
@@ -16,5 +17,65 @@ window.onload = function () {
     };
     
     mediaCross.addEventListener("click", mediaClose, false);
+    
+    function wScroll(){
+        console.log(messageFeed.scrollTop);
+        
+        messageHeight = messageFeed.offsetHeight;
+    
+        var heightToScroll = (130);
+        var header = document.getElementById('header');
+    
+        if(messageFeed.scrollTop > heightToScroll){
+            header.className = "header header-scrolled"
+            messageFeed.className = "message-feed message-feed-scrolled"
+        }
+        else if(messageFeed.scrollTop < 1){
+            header.className = "header header-unscrolled"
+            messageFeed.className = "message-feed"
+        }
+     };
+    
+    messageFeed.addEventListener("scroll", wScroll, false);
+    
+    wScroll();
+        
+        
+    function logMessage(event){
+        
+        //get time
+    
+        var d = new Date();    
+        var h = d.getHours();
+        var m = d.getMinutes(); 
+        
+        //build message
+        
+        function appendMessage() {
+            $('#message-feed').append(
+                '<div class="message message-to"><div class="message-name"><h1>You</h1></div><div class="message-body"><p>' + messageVal + '</p></div><div class="message-timestamp"><p>Today ' + h + ' : ' + m + '</p></div>'   
+            );
+        }
+        
+        //set event trigger
+        
+        var messageInput = document.getElementById("message-input");
+        
+        var messageVal = messageInput.value;
+        
+        //fire on enter
+        
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            document.getElementById("output").innerHTML = messageVal;
+            messageInput.value= "";
+            appendMessage();
+            messageFeed.scrollTop = messageFeed.scrollHeight;
+        }
+    };
+        
+    document.addEventListener("keypress", logMessage, false);
+
+    
 }
 
